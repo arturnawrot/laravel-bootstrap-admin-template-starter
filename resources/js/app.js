db7ec2cba38079335016a0c6f8aa61f580ed775c
Arturek1 <artur.programista2@gmail.com>
2020-05-10 12:32:59 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+require('bootstrap/dist/js/bootstrap.bundle');
+
+require('chart.js');
+
+require('jquery.easing');
+
+require('datatables.net-bs4');
+
+require('./sb-admin-2');
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ isset($title) ? $title.' | ' : '' }}{{ config('app.name') }}</title>
+
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body id="page-top">
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            @include('layouts._sidebar')
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+                <!-- Main Content -->
+                <div id="content">
+                    @include('layouts._navbar')
+                        <!-- Begin Page Content -->
+                        <div class="container-fluid">
+                        <!-- Page Heading -->
+                            <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                            @yield('main')
+
+                        </div>
+                        <!-- /.container-fluid -->
+                </div>
+                <!-- End of Main Content -->
+                @include('layouts._footer')
+            </div>
+            <!-- End of Content Wraper -->
+        </div>
+        <!-- End of Page Wrapper -->
+
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </body>
+</html>
